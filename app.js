f17197dc54f57f88ec1abe7f1c664573b4a75357
badri <badri82301@gmail.com>
2022-07-23 22:34:32 +0530

    testing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            
-    optionSuccessStatus:200
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions));
 const authRoute = require('./routes/authRoute')
@@ -12,16 +12,18 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('dotenv/config')
 
-app.use(bodyParser.json({urlencoded:true}))
-app.use("/auth",authRoute)
-
-mongoose.connect(process.env.MONGODB,(err)=>{
-    if(!err)
+app.use(bodyParser.json({ urlencoded: true }))
+app.use("/auth", authRoute)
+app.get("/", (req, res) => {
+    res.send("hii")
+})
+mongoose.connect(process.env.MONGODB, (err) => {
+    if (!err)
         console.log("Database Connected")
     else
         console.log(err)
 })
-app.listen(process.env.PORT,(err)=>{
-    if(!err)
+app.listen(process.env.PORT, (err) => {
+    if (!err)
         console.log("Server Started")
 })
