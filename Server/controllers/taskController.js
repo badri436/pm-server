50dd201f3c76604e6781c2791e080475eb8e5af3
badri <badri82301@gmail.com>
2022-08-10 12:36:53 +0530

    tasklist

--- Server/controllers/taskController.js
+++ Server/controllers/taskController.js
@@ -8,7 +8,7 @@ const create = async (req, res) => {
     try {
         const { taskName, description, assignTo, projectId, startDate, endDate, priority, taskListId } = req.body
         const { userId } = req.user
-    
+
 
         const newTask = new task({
             userId,
@@ -21,84 +21,84 @@ const create = async (req, res) => {
             taskStatus: "Open",
             priority
         })
-    
+
         await newTask.save();
 
-        await milestoneTaskList.findByIdAndUpdate(taskListId,{
-            $push:{
-                taskId:newTask._id
+        await milestoneTaskList.findByIdAndUpdate(taskListId, {
+            $push: {
+                taskId: newTask._id
             }
         })
-    
+
         return res.status(200).json({
-            "status":true,
-            "data":"Task Created Successfully"
+            "status": true,
+            "data": "Task Created Successfully"
         })
     } catch (error) {
         return res.status(400).json({
-            "status":false,
-            "message":"Task Creation Failed!"
+            "status": false,
+            "message": "Task Creation Failed!"
         })
     }
 }
 
-const listBasedOnUser = async(req,res)=>{
-    try{
-        const { userId } = req.user
-        const {count, page} = req.query
-        const skip = count*page
-        const getTask = await task.find({status:1, userId:userId }).populate({ path: "projectId", model: "project", select: { _id: 1, projectName: 1 } }).populate({ path: "assignTo", model: "user", select: { _id: 1, name: 1 } }).limit(Number(count)).skip(Number(skip))
-        const getTaskCount = await task.find({status:1, userId:userId}).count()
-        return res.status(200).json({
-            "status":true, 
-            "data":getTask,
-            "totalCount":getTaskCount
-        })
-    }
-    catch(error){
-        return res.status(400).json({
-            "status":false,
-            "message":error
-        })
-    }
-}
-
-const listBasedOnProject = async(req,res)=>{
-    try{
-        const {projectId} = req.body
-        
-
-        const getTask = await task.find({status:1, projectId:projectId })
-        return res.status(200).json({
-            "status":true,
-            "data":getTask
-        })
-    }
-    catch(error){
-        return res.status(400).json({
-            "status":false,
-            "message":error
-        })
-    }
-}
-
-const groupByStatus = async(req,res) => {
+const listBasedOnUser = async (req, res) => {
     try {
-        const {projectId} = req.body
+        const { userId } = req.user
+        const { count, page } = req.query
+        const skip = count * page
+        const getTask = await task.find({ status: 1, userId: userId }).populate({ path: "projectId", model: "project", select: { _id: 1, projectName: 1 } }).populate({ path: "assignTo", model: "user", select: { _id: 1, name: 1 } }).limit(Number(count)).skip(Number(skip))
+        const getTaskCount = await task.find({ status: 1, userId: userId }).count()
+        return res.status(200).json({
+            "status": true,
+            "data": getTask,
+            "totalCount": getTaskCount
+        })
+    }
+    catch (error) {
+        return res.status(400).json({
+            "status": false,
+            "message": error
+        })
+    }
+}
+
+const listBasedOnProject = async (req, res) => {
+    try {
+        const { projectId } = req.body
+
+
+        const getTask = await task.find({ status: 1, projectId: projectId })
+        return res.status(200).json({
+            "status": true,
+            "data": getTask
+        })
+    }
+    catch (error) {
+        return res.status(400).json({
+            "status": false,
+            "message": error
+        })
+    }
+}
+
+const groupByStatus = async (req, res) => {
+    try {
+        const { projectId } = req.body
         const filter = await task.aggregate()
-        .match({
-            projectId:mongoose.Types.ObjectId(projectId)
-        })
-        .group({
-            _id:"$taskStatus",
-            taskList:{
-                $push:"$$ROOT"
-            }
-        })
+            .match({
+                projectId: mongoose.Types.ObjectId(projectId)
+            })
+            .group({
+                _id: "$taskStatus",
+                taskList: {
+                    $push: "$$ROOT"
+                }
+            })
         console.log(filter)
         return res.status(200).json({
-            "status":true,
-            "data":filter
+            "status": true,
+            "data": filter
         })
 
     } catch (error) {
@@ -109,53 +109,53 @@ const groupByStatus = async(req,res) => {
     }
 }
 
-const groupByProject = async(req,res) => {
+const groupByProject = async (req, res) => {
     try {
-        const {userId} = req.user
+        const { userId } = req.user
         const { page, count } = req.query
         const skip = page * count
 
         const { projectId } = req.body
         const filter = await task.aggregate()
-        .lookup({
-            from: "projects",
-            localField: "projectId",
-            foreignField: "_id",
-            as: "projectDetails"
-        })
-        .lookup({
-            from: "users",
-            localField: "assignTo",
-            foreignField: "_id",
-            as: "assignToDetails"
-        })
-        .unwind({path:"$projectDetails"})
-        .unwind({path:"$assignToDetails"})
-        .project({
-            "_id":1,
-            "taskName":1,
-            "description":1,
-            "projectId":1,
-            "taskStatus":1,
-            "assignTo":1,
-            "startDate":1,
-            "endDate":1,
-            "priority":1,
-            "createdAt":1,
-            "updatedAt":1,
-            "projectDetails.projectName":1,
-            "assignToDetails.name":1
-        })
-        .group({
-            _id:"$projectId",
-            taskList:{
-                $push:"$$ROOT"
-            }
-        })
-        .sort({ "createdAt": -1 })
-        .facet({
-            data: [{ $skip: Number(skip) }, { $limit: Number(count) }]
-        })
+            .lookup({
+                from: "projects",
+                localField: "projectId",
+                foreignField: "_id",
+                as: "projectDetails"
+            })
+            .lookup({
+                from: "users",
+                localField: "assignTo",
+                foreignField: "_id",
+                as: "assignToDetails"
+            })
+            .unwind({ path: "$projectDetails" })
+            .unwind({ path: "$assignToDetails" })
+            .project({
+                "_id": 1,
+                "taskName": 1,
+                "description": 1,
+                "projectId": 1,
+                "taskStatus": 1,
+                "assignTo": 1,
+                "startDate": 1,
+                "endDate": 1,
+                "priority": 1,
+                "createdAt": 1,
+                "updatedAt": 1,
+                "projectDetails.projectName": 1,
+                "assignToDetails.name": 1
+            })
+            .group({
+                _id: "$projectId",
+                taskList: {
+                    $push: "$$ROOT"
+                }
+            })
+            .sort({ "createdAt": -1 })
+            .facet({
+                data: [{ $skip: Number(skip) }, { $limit: Number(count) }]
+            })
             .match({
                 projectId: mongoose.Types.ObjectId(projectId)
             })
@@ -171,19 +171,35 @@ const groupByProject = async(req,res) => {
         }).group({
             _id: "$projectId",
         }).count("totalcount")
-        
+
         return res.status(200).json({
-            "status":true,
-            "data":filter[0].data,
-            "totalCount":taskCount[0].totalcount
+            "status": true,
+            "data": filter[0].data,
+            "totalCount": taskCount[0].totalcount
         })
 
-        } catch (error) {
-            return res.status(400).json({
-                "status":false,
-                "message":"Failed"
-            })
-        }
+    } catch (error) {
+        return res.status(400).json({
+            "status": false,
+            "message": "Failed"
+        })
     }
-    
-    module.exports = {create, listBasedOnUser, listBasedOnProject, groupByStatus, groupByProject}
+}
+
+const individualTaskList = async (req, res) => {
+    try {
+        const { taskId } = req.body
+        const getTask = await task.findById(taskId).populate({ path: "projectId", model: "project", select: { _id: 1, projectName: 1 } }).populate({ path: "assignTo", model: "user", select: { _id: 1, name: 1 } })
+        return res.status(200).json({
+            "status": true,
+            "data": getTask
+        })
+    } catch (error) {
+        return res.status(400).json({
+            "status": false,
+            "message": "Failed"
+        })
+    }
+}
+
+module.exports = { create, listBasedOnUser, listBasedOnProject, groupByStatus, groupByProject, individualTaskList }
--- Server/routes/taskRoute.js
+++ Server/routes/taskRoute.js
@@ -3,13 +3,13 @@ const taskRoute = express.Router()
 const taskController = require('../controllers/taskController')
 const middle = require('../middleware/middleware')
 
+taskRoute.get("/groupbyproject", middle, taskController.groupByProject)
+taskRoute.get("/listbasedonuser", middle, taskController.listBasedOnUser)
+taskRoute.get("/listbasedonproject", middle, taskController.listBasedOnProject)
+taskRoute.post("/create", middle, taskController.create)
+taskRoute.post("/individualTask", middle, taskController.individualTaskList)
+taskRoute.post("/groupbystatus", middle, taskController.groupByStatus)
 
-taskRoute.get("/listbasedonuser",middle,taskController.listBasedOnUser)
-taskRoute.get("/listbasedonproject",middle,taskController.listBasedOnProject)
-taskRoute.post("/create",middle,taskController.create)
-
-taskRoute.post("/groupbystatus",middle,taskController.groupByStatus)
-taskRoute.get("/groupbyproject",middle,taskController.groupByProject)
 
 
 module.exports = taskRoute
